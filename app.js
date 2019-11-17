43a4e1c40cdca493b2790e81056ea7560077b977
quanghai29 <lequanghai540@gmail.com>
2019-11-17 17:28:17 +0700

    create complete version1

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('hbs', exphbs(
+    {
+        defaultLayout: 'main.hbs',
+        layoutsDir: 'views/_layouts'
+    }));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(3000);
