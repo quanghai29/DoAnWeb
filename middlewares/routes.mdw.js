8c042c5c3bd403010e5c5e794888bcb851f6027f
QUANG HAI <lequanghai540@gmail.com>
2019-12-23 22:34:44 +0700

    view myproduct

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,13 +1,13 @@
 //const restrict = require('../middlewares/auth.mdw');
 
 module.exports = function (app) {
-  //app.use('/demo', require('../routes/demo.route'));
-  app.use('/account', require('../routes/account.route'));
-  app.use('/user', require('../routes/user/profile.route'));
-  app.use('/user', require('../routes/user/upgrade.route'));
-  //app.use('/user', require('../routes/user/orders.route'));
-   app.use('/user', require('../routes/user/products.route'));
-  //app.use('/categories', require('../routes/category.route'));
-  //app.use('/products', require('../routes/product.route'));
-  //app.use('/admin/categories', restrict, require('../routes/admin/category.route'));
-};
+    //app.use('/demo', require('../routes/demo.route'));
+    app.use('/account', require('../routes/account.route'));
+    app.use('/user', require('../routes/user/profile.route'));
+    app.use('/user', require('../routes/user/upgrade.route'));
+    //app.use('/user', require('../routes/user/orders.route'));
+     app.use('/user', require('../routes/user/products.route'));
+    //app.use('/categories', require('../routes/category.route'));
+    //app.use('/products', require('../routes/product.route'));
+    //app.use('/admin/categories', restrict, require('../routes/admin/category.route'));
+  };
