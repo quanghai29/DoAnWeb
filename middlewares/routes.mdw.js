ed1c24ff591d3e1c576d1740270991f446680d2f
DŨNG BÙI TIẾN <1712365@student.hcmus.edu.vn>
2019-12-31 10:06:54 +0700

    them Listusers

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -11,4 +11,5 @@ module.exports = function (app) {
   //app.use('/categories', require('../routes/category.route'));
   //app.use('/admin/categories', restrict, require('../routes/admin/category.route'));
   app.use('/admin/categories', require('../routes/admin/category.route'));
+  app.use('/admin/users',require('../routes/admin/user.route'));
 };
--- models/user.model.js
+++ models/user.model.js
@@ -2,15 +2,15 @@ const db = require('../utils/db');
 
 module.exports = {
   all: () => db.load("select * from member"),
+  allUser: () => db.load("SELECT m.f_ID, m.f_Username, l.ten_loai FROM member m,loai_member l WHERE m.ID_loai_member!=3 AND m.ID_loai_member=l.id"),
   allUserNames: async username => {
-    const rows=await db.load(`select f_Username from member where f_Username='${username}'`);
-    if(rows.length===0)
-    {
+    const rows = await db.load(`select f_Username from member where f_Username='${username}'`);
+    if (rows.length === 0) {
       return false;
     }
     return true;
   },
-//   single: id => db.load(`select * from member where f_Id = ${id}`),
+  //   single: id => db.load(`select * from member where f_Id = ${id}`),
   singleByUsername: async username => {
     const rows = await db.load(`select * from member where f_Username = '${username}'`);
     if (rows.length === 0)
