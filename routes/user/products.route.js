dd0a1911abfa52aa673c67d5b24506fafd5f2aa4
QUANG HAI <lequanghai540@gmail.com>
2020-01-05 20:38:55 +0700

    auto place

--- routes/user/products.route.js
+++ routes/user/products.route.js
@@ -123,9 +123,11 @@ router.post('/upload_image/:id', function (req, res) {
     });
 })
 
-//---Đấu Giá
-//Update Giá đặt mới 
+//---Đấu Giá Bằng Tay
 router.post('/product/PlaceBid/:id',async (req, res) => {
+
+    //Nếu đã có dữ liệu đấu giá rồi thì load lên lịch sử đấu giá của người đang đăng nhập
+    var hisOfferBidder = await productModel.getOfferBidder(req.params.id,req.session.authUser.f_ID);
 
     var newPlaceBid= numeral(req.body.PlaceBid).value();//giá đặt của người ra giá
     var StatusCurrentWinner = 0;//Trạng thái đấu giá của người đang đấu giá
@@ -150,7 +152,7 @@ router.post('/product/PlaceBid/:id',async (req, res) => {
         console.log(Step);
 
         //Nếu giá đặt sau cao hơn giá đang giữ công với bước giá
-        if(newPlaceBid > (BestPrice + Step))
+        if(newPlaceBid >= (BestPrice + Step))
         {
             console.log("Update status");
             //Cập nhật lại trạng thái giữ giá của người đang ra giá
@@ -170,14 +172,15 @@ router.post('/product/PlaceBid/:id',async (req, res) => {
             res.redirect(`/categories/product/${req.params.id}`);
         }
     }
-    else{//Nếu chưa có người ra giá thì cập nhật trạng thái giữ giá của ngươi đang ra giá
+    else{
+        //Nếu chưa có người ra giá thì cập nhật trạng thái giữ giá của ngươi đang ra giá
         StatusCurrentWinner=1;
     }
 
    
-
     //Nếu chưa có ai ra giá thì so sáng với giá khởi didemr  //xử lý ở client
 
+    //Nếu người thắng cuộc là người đã đáu giá trước đó thì 
 
     //Thêm vào model ra giá sản phẩm
     var entity={
@@ -188,12 +191,132 @@ router.post('/product/PlaceBid/:id',async (req, res) => {
         StatusWinner: StatusCurrentWinner
     };
     console.log(entity);
-    const results = await productModel.addOfferProduct(entity);
+    
+    //nếu chưa có lịch sử đấu giá của người đăng nhập 
+    if(hisOfferBidder.length ===0)
+    {
+        //Insert
+        const resultsInsert = await productModel.addOfferProduct(entity);
+    }
+    else//ngược lại chỉ cần update
+    {
+        const resultsUpdate = await productModel.updateStatusWinner(entity);
+    }
 
     //quay lại trang đang xem chi tiết sản phẩm
     res.redirect(`/categories/product/${req.params.id}`);
 })
 
+
+//Đấu Gía tự động
+router.post('/product/autoPlaceBid/:id',async (req, res) => {
+
+     //Nếu đã có dữ liệu đấu giá rồi thì load lên lịch sử đấu giá của người đang đăng nhập
+    var hisOfferBidder = await productModel.getOfferBidder(req.params.id,req.session.authUser.f_ID);
+    console.log(hisOfferBidder);
+    if(hisOfferBidder.length !== 0)
+    {
+         //nếu vẫn đang là người thắng cuộc thì không làm gì cả
+        if(hisOfferBidder[0].StatusWinner===1)
+        {
+            return res.redirect(`/categories/product/${req.params.id}`);
+        }
+    }
+
+    var StatusCurrentWinner = 0;//Trạng thái giữ giá của người đang đăng nhập
+    var newPlaceBid = null ;//giá đặt của hệ thống 
+    var maxBid=numeral(req.body.MaxBidLabel).value();//giá cao nhất mà Bidder có thể trả
+
+    console.log(maxBid);
+    //Người đang giữ giá cao nhất trước đó
+    var BestPricePrevious = await productModel.getBestPricePrevious(req.params.id);
+    var ProductPrice = await productModel.getProductPrice(req.params.id);
+
+    console.log(BestPricePrevious);
+    console.log(ProductPrice);
+    
+
+    //Nếu đã có người ra giá trước đó
+    if(BestPricePrevious.length !== 0)
+    {
+        
+        var BestPrice =  BestPricePrevious[0].PricePlaceBid;//Gía đang giữ
+        var Step =  ProductPrice[0].PriceStep;//Bước giá
+
+        console.log(newPlaceBid);
+        console.log(BestPrice);
+        console.log(Step);
+
+        //Nếu giá max cao hơn so với giá đang giũ + bước giá
+        if(maxBid >= (BestPrice + Step))
+        {
+            //Đặt lại giá vừa đủ để thắng
+            newPlaceBid=BestPrice + Step;
+            console.log("Update status");
+            //Cập nhật lại trạng thái giữ giá của người đang đăng nhập
+            StatusCurrentWinner=1;
+
+            // Cập nhật lại trạng thái giữ giá của người đã giữ giá trước đó
+            const entityStatusWinner = {
+                ProID: req.params.id,
+                Bidder: BestPricePrevious[0].Bidder,
+                StatusWinner : 0
+            };
+            console.log(entityStatusWinner);
+            const UpdateStatusWinner = await productModel.updateStatusWinner(entityStatusWinner);
+
+        }
+        else{//nếu giá max không cao hơn thì không lưu vào csdl
+            //cho giá max, giá đặt về null, StatusCurrentWinner = 0
+            maxBid=null;
+            newPlaceBid=null;
+            StatusCurrentWinner=0;
+        }
+    }
+    else{
+        //Nếu chưa có người ra giá 
+        //Đặt lại giá vừa đủ (giá khởi điểm +  bước giá)
+        newPlaceBid = ProductPrice[0].PriceBegin + ProductPrice[0].PriceStep;
+
+        //Đặt trạng thái giữ giá
+        StatusCurrentWinner=1;
+    }
+
+    
+    var entity={
+        ProID: req.params.id,
+        Bidder: req.session.authUser.f_ID,
+        PricePlaceBid: newPlaceBid,
+        MaxBid: maxBid,
+        TimePlace: moment().format('YYYY-MM-DD HH:mm:ss'),
+        StatusWinner: StatusCurrentWinner
+    };
+    console.log(entity);
+
+    //nếu chưa có lịch sử đấu giá của người đăng nhập 
+    if(hisOfferBidder.length ===0)
+    {
+        //Insert
+        const resultsInsert = await productModel.addOfferProduct(entity);
+    }
+    else//ngược lại chỉ cần update
+    {
+        const resultsUpdate = await productModel.updateStatusWinner(entity);
+    }
+    //res.send('ok');
+    res.redirect(`/categories/product/${req.params.id}`);
+})
+
+router.post('/product/offAutoPlaceBid/:id',async (req, res) => {
+    var entity={
+        ProID: req.params.id,
+        Bidder: req.session.authUser.f_ID,
+        MaxBid: null,
+        TimePlace: moment().format('YYYY-MM-DD HH:mm:ss')
+    };
+    console.log(entity);
+    const resultsUpdate = await productModel.updateStatusWinner(entity);
+})
 // //edit dữ liệu của database
 // router.get('/edit/:id', async (req, res) => {
 //     const rows = await productModel.getPro(req.params.id);
